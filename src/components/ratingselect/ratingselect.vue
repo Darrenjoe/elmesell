098291144e42578d408922ca5c53c2293a490119
Darren <491361994@qq.com>
2018-10-30 21:15:58 +0800

    first commit

---
+++ src/components/ratingselect/ratingselect.vue
@@ -0,0 +1,20 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  props: {
+    ratings: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
